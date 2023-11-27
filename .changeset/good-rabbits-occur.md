4ba8a583a0f629999cf8db28b3fe9180ff2d0000
Landon Thull <landonthull.tx@gmail.com>
2023-11-26 16:40:07 -0800

    add tMinus function

---
+++ .changeset/good-rabbits-occur.md
@@ -0,0 +1,5 @@
+---
+"t-minus-time": major
+---
+
+Add tMinus function
--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
-export const add = (a: number, b: number) => {
-  return a + b
+export const tMinus = (end: Date, start?: Date): String => {
+  // seconds between two events
+  let delta = (start ? end.getTime() - start.getTime() : end.getTime() - Date.now()) / 1000;
+
+  // event in past (+) or future (-)
+  let sign = delta < 0 ? "+" : "-";
+
+  if (delta < 0) {
+    delta = -delta;
+  }
+
+  let hr = Math.floor(delta / 3600);
+  let min = Math.floor(delta / 60) % 60;
+  let sec = Math.floor(delta % 60);
+
+  let hrStr = hr < 10 ? `0${hr}` : `${hr}`;
+  let minStr = min < 10 ? `0${min}` : `${min}`;
+  let secStr = sec < 10 ? `0${sec}` : `${sec}`;
+
+  // format - HH:MM:SS
+  let result = `T${sign}${hrStr}:${minStr}:${secStr}`;
+
+  return result;
 }
